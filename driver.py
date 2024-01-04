6a0cae6a6bd6db35cd1a0ed7fb65f1f56f8768e4
Jiale Chen <55820495+jiale0402@users.noreply.github.com>
2024-01-04 15:52:38 -0600

    rewrite doc string

--- driver.py
+++ driver.py
@@ -12,9 +12,19 @@ if __name__ == "__main__":
         eid:            str, exchange id
         dest:           str, absolute path to the directory to store the output
         frequency:      timedelta, frequency of data replay
-        universe:       list, string codes of all instruments, if None, will be inferred from data
+        universe:       list, string codes of all instruments, if None, will be inferred from data of start date
         buffer_size:    int, buffer size of the output file streams
         start:          str, start date of the data to be replayed, format: YYYY-MM-DD
+
+        Output:
+        -------
+        One csv file (in the destination directory) for each specified symbol in the universe with the following columns:
+        bid_price_0 ... bid_price_9
+        bid_qty_0   ... bid_qty_9
+        ask_price_0 ... ask_price_9 
+        ask_qty_0   ... ask_qty_9
+        open, high, low, close, volue, amount
+        timestamp
         
     """
     r = Replayer(
