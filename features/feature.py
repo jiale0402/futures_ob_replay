78b5ccf045fcedd6ba080547c25d34859b0b6443
Jiale Chen <55820495+jiale0402@users.noreply.github.com>
2024-01-07 19:58:50 -0600

    Update feature.py

--- features/feature.py
+++ features/feature.py
@@ -2,6 +2,10 @@ from abc import ABC, abstractmethod
 
 class Feature(ABC):
 
+    """
+    this is an abstract template of features
+    """
+
     def init(self, name, look_back_window):
         raise NotImplementedError("Init must be overrided")
 
