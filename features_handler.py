e13c0c3f522db3f401ff150edf3dc2dd3b4222d3
Jiale Chen <55820495+jiale0402@users.noreply.github.com>
2024-01-07 20:33:40 -0600

    Update features_handler.py

--- features_handler.py
+++ features_handler.py
@@ -8,10 +8,11 @@ class FeaturesHandler:
         ob_depth: int = 10
     ):
         self.features = features
-        self.look_back = max([f.look_back for f in features])
+        self.look_back = max([f.lookback for f in features])
         self.cached_history = np.zeros((10, 10))
         self.ob_depth = ob_depth
         self.names = [f.name for f in features]
+        self.mask = np.array([f.written for f in features])
 
     def compute(
         self,
@@ -23,10 +24,9 @@ class FeaturesHandler:
             ob_end_idx = self.ob_depth * 4
             snapshot = current_input[:ob_end_idx]
             ohlcva = current_input[ob_end_idx:ob_end_idx+6]
-
             computed_features = []
             for i, feature in enumerate(self.features):
-                computed_features += [feature.compute(snapshot, ohlcva, self.cached_history[:, i])]
-
-            result[symbol] = computed_features
+                lookback = self.cached_history[:, i]
+                computed_features += [feature.compute(snapshot, ohlcva, lookback)]
+            result[symbol] = np.array(computed_features)[self.mask]
         return result
