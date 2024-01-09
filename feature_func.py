aabfc023c60f6d43d6127437f0fbf2eedd036e2c
jiale0402 <55820495+jiale0402@users.noreply.github.com>
2024-01-08 20:39:29 -0600

    update

---
+++ feature_func.py
@@ -0,0 +1,60 @@
+import numpy as np 
+
+all_features = ['oir', 'mpb', 'voi']
+all_feature_funcs = [oir, mpb, voi]
+levels = 5
+arr = np.array([1, 0.8, 0.6, 0.4, 0.2])
+sum_arr = arr.sum()
+cum_sum_arr = np.cumsum(arr)
+
+def oir(data, prev_data):
+    if prev_data is None: return None
+    bid_volumes = np.array(data[10:15])
+    ask_volumes = np.array(data[30:35])
+    bid_volumes = np.cumsum(bid_volumes * arr)
+    ask_volumes = np.cumsum(ask_volumes * arr)
+    return ((bid_volumes - ask_volumes) / (bid_volumes + ask_volumes)).sum()
+
+def mpb(data, prev_data):
+    if prev_data is None: return None
+    bid_1, ask_1 = data[0], data[20]
+    midp = (bid_1 + ask_1) / 2
+    prev_bid_1, prev_ask_1 = prev_data[0], prev_data[20]
+    prev_midp = (prev_bid_1 + prev_ask_1) / 2
+    mt = (midp + prev_midp) / 2
+    ohlcva = data[-6:]
+    vwap = ohlcva[-1] / ohlcva[-2]
+    return vwap - mt
+
+def voi(data, prev_data):
+    if prev_data is None: return None
+    delta_bid = np.zeros(levels)
+    delta_ask = np.zeros(levels)
+    
+    for i in range(levels):
+        prev_bid_i = prev_data[i]
+        prev_ask_i = prev_data[i + 20]
+        bid_i = data[i]
+        ask_i = data[i + 20]
+        
+        if bid_i > prev_bid_i:
+            delta_bid[i] = data[10 + i]
+        elif bid_i < prev_bid_i:
+            delta_bid[i] = 0
+        else:
+            delta_bid[i] = data[10 + i] - prev_data[10 + i]
+        
+        if ask_i > prev_ask_i:
+            delta_ask[i] = 0
+        elif ask_i < prev_ask_i:
+            delta_ask[i] = data[30 + i]
+        else:
+            delta_ask[i] = data[30 + i] - prev_data[30 + i]
+        
+        delat_bid[i] = delat_bid[i] * arr[i]
+        delta_ask[i] = delta_ask[i] * arr[i]
+    
+    delta_bid = np.cumsum(delta_bid)
+    delta_ask = np.cumsum(delta_ask)
+    
+    return ((delta_bid - delta_ask) / cum_sum_arr).sum()
--- replayer.py
+++ replayer.py
@@ -9,6 +9,7 @@ from orderbook import LocalOrderBook
 import copy
 from trades import TradesHandler
 from concurrent.futures import ProcessPoolExecutor, as_completed
+from feature_func import all_feature_funcs, all_features
         
 # separated from class to allow for parallel processing
 def _compute_day(
@@ -23,6 +24,7 @@ def _compute_day(
     ) -> None:    
     dest = open(dest, 'a', buffering=buffer_size) 
     # replay loop
+    prev_data = None
     for (l2_updates, trades) in zip(l2.iter_rows(named = True), l1.iter_rows(named = True)):
         # assure time is uniform
         timestamp = l2_updates.pop('Timestamp')
@@ -43,7 +45,10 @@ def _compute_day(
         # record the features
         data = ob_handler.take_snapshot()
         data += trade_handler.get_ohlcva()
+        data += [f(data, prev_data) for f in all_feature_funcs]
         dest.write(f"{str(data)[1:-1]}, {timestamp}\n")
+        prev_data = data
+        
     dest.close()
             
 def handle_trades(row, l1_col_mapping, trades_handler) -> None: # message handler wrapper
@@ -131,23 +136,22 @@ def handle_DeltaRefresh(row, ob, l2_col_mapping):
     elif action == "10.0":  # 1.4.2 10 - AskRemoveLevelAndAppend
         ob.AskRemoveLevelAndAppend(level, price, qty)
         
-
 class Replayer:
-    
+
     def __init__(
-            self, 
-            src: str, 
-            eid: str, 
+            self,
+            src: str,
+            eid: str,
             dest: str,
             start: str = None,
             frequency: datetime.timedelta = datetime.timedelta(milliseconds=100),
             universe: list = [],
             buffer_size: int = 2**20,
-            max_workers: int = 2
+            max_workers: int = 2,
         ) -> None:
-        """ 
+        """
         main thread of the feature generation process
-        
+
         Params:
         -------
         path:           str, absolute path to the directory containing the data
@@ -175,7 +179,11 @@ class Replayer:
         self.blank_update_template = {k:[] for k in L2_SCHEMA.keys()}
         self.blank_trade_template = {k:[] for k in L1_SCHEMA.keys()}
         self.max_workers = max_workers
-        
+
+    def close(self):
+        for dest in self.dest_file_streams.values():
+            dest.close()
+
     def compute_day(self):
         """
         computes one day worth of features and write to destination
@@ -192,18 +200,17 @@ class Replayer:
                     self.l1_col_mapping,
                     self.ob_container[code],
                     self.trade_handler_container[code],
-                    self.dest_file_streams[code],
-                    self.buffer_size
                 )]
-            
-            # catch exceptions & print progress
-            for i, future in enumerate(as_completed(rs)):
-                try:
-                    _ = future.result()
-                    print(f"Finished {self.universe[i] + ' ' + self.date}")
-                except Exception as exc:
-                    print(exc)
-        
+
+        outputs = {}
+        # catch exceptions & print progress
+        for i, future in enumerate(as_completed(rs)):
+            try:
+                outputs[self.universe[i]], timestamp = future.result()
+                print(f"Finished {self.universe[i] + ' ' + self.date}")
+            except Exception as exc:
+                print(exc)
+
     def _read_next_date(self) -> None:
         # read next date's data
         try:
@@ -211,21 +218,21 @@ class Replayer:
         except:
             raise ValueError("No more data to be replayed")
         date, dir, eid = self.date, self.dir, self.eid
-        
+
         # load l2 data
         self.curr_data.clear()
         self.curr_data['date'] = date
         self.curr_data['l2'] =  pl.read_csv(
-            gzip.open(os.path.join(dir, f"{date}_{eid}_L2.csv.gz"), 'rb').read(), 
+            gzip.open(os.path.join(dir, f"{date}_{eid}_L2.csv.gz"), 'rb').read(),
             schema=L2_SCHEMA
         )
-                         
+
         # load l1 data
         self.curr_data['trades'] = pl.read_csv(
             gzip.open(os.path.join(dir, "trades", f"{date}_{eid}_L1-Trades.csv.gz"), 'rb').read(),
             schema=L1_SCHEMA
         )
-        
+
         # filter out instruments not in the universe
         if self.universe != []:
             self.curr_data['l2'] = self.curr_data['l2'].filter(
@@ -234,11 +241,11 @@ class Replayer:
             self.curr_data['trades'] = self.curr_data['trades'].filter(
                 pl.col('Code').is_in(self.universe) | pl.col('Code').eq('blank')
             )
-        
+
         # cast the string timestamps to datetime objects
         self.curr_data['l2'] = self.curr_data['l2'].with_columns(
             Timestamp = pl.coalesce(
-            ['DeltaRefresh_ServerTimestamp', 
+            ['DeltaRefresh_ServerTimestamp',
              'OverlapRefresh_ServerTimestamp'])
             .str.split(".")
             .list
@@ -256,26 +263,23 @@ class Replayer:
             .dt.with_time_unit("us")
             .alias("Timestamp")
         )
-        
+
         print(f"finished loading data for {self.date}")
 
+        if len(self.ob_container) == 0:
+            self.init_params()
         l2_schema = copy.deepcopy(L2_SCHEMA)
         l2_schema['Timestamp'] = pl.Datetime
         l1_schema = copy.deepcopy(L1_SCHEMA)
         l1_schema['Timestamp'] = pl.Datetime
         self.time = datetime.datetime.strptime(date, "%Y-%m-%d") - datetime.timedelta(hours=2)
         blank_update = copy.deepcopy(self.blank_update_template)
-        blank_update['Timestamp'] = self.time + datetime.timedelta(days=1) - self.freq / 2
         blank_trade = copy.deepcopy(self.blank_trade_template)
-        blank_trade['Timestamp'] = self.time + datetime.timedelta(days=1) - self.freq / 2
-        # initialize universe and column mapping on first date
-        if len(self.ob_container) == 0:
-            self.init_params()
         self._insert_to_end(blank_update, blank_trade, l2_schema, l1_schema, date)
         self._insert_to_start(blank_update, blank_trade, l2_schema, l1_schema)
-        
+
         print(f"finished blank l1/l2 message insertion for {self.date}")
-        
+
         # sanity check
         msg = f"l2 and l1 dataframes have different min timestamps: {self.curr_data['l2']['Timestamp'].min()},\
                 {self.curr_data['trades']['Timestamp'].min()}"
@@ -284,22 +288,22 @@ class Replayer:
                 {self.curr_data['trades']['Timestamp'].max()}"
         assert self.curr_data['l2']['Timestamp'].max() == self.curr_data['trades']['Timestamp'].max()\
                 == self.time + datetime.timedelta(hours=24) - self.freq / 2, msg
-                
-        
+
+
         # partition all data by instrument for parallel processing
         self.curr_data['l2'] = self.curr_data['l2'].partition_by(
-            by='Code', 
+            by='Code',
             maintain_order=True,
-            include_key=True, 
+            include_key=True,
             as_dict=True
         )
         self.curr_data['trades'] = self.curr_data['trades'].partition_by(
-            by='Code', 
+            by='Code',
             maintain_order=True,
-            include_key=True, 
+            include_key=True,
             as_dict=True
         )
-        
+
         print(f"finished partitioning by instrument code for {self.date}")
 
         # aggregating and upsampling to make the data time uniform
@@ -355,21 +359,21 @@ class Replayer:
                     time_column="Timestamp",
                     every=self.freq
                 )
-                
+
         if self.l2_col_mapping is None:
             self.l2_col_mapping = {col: i for i, col in enumerate(self.curr_data['l2'][self.universe[0]].columns[1:])}
             self.l1_col_mapping = {col: i for i, col in enumerate(self.curr_data['trades'][self.universe[0]].columns[1:])}
             # the first column is the timestamp, which is not in the schema
-            
+
         shapes = [self.curr_data['l2'][code].shape[0] for code in self.universe]
         assert len(set(shapes)) == 1, f"l2 dataframes have different shapes: {shapes}"
         shapes = [self.curr_data['trades'][code].shape[0] for code in self.universe]
         assert len(set(shapes)) == 1, f"l1 dataframes have different shapes: {shapes}"
-        
+
         print(f"finished preprocessing for date {self.date}")
 
     def _insert_to_end(self, blank_update, blank_trade, l2_schema, l1_schema, date):
-        # insert a blank message to end of trades/l2 data 
+        # insert a blank message to end of trades/l2 data
         # to ensure uniform sampling
         self.curr_data['l2'] = self.curr_data['l2'].set_sorted(
             'Timestamp',
@@ -378,8 +382,8 @@ class Replayer:
             pl.col('Timestamp') < self.time + datetime.timedelta(days=1)
         )
         eod_time = [self.time + datetime.timedelta(days=1) - self.freq/2]
-        blank_trade['Timestamp'] = [eod_time] * len(self.universe)
-        blank_update['Timestamp'] = [eod_time] * len(self.universe)
+        blank_trade['Timestamp'] = eod_time * len(self.universe)
+        blank_update['Timestamp'] = eod_time * len(self.universe)
         self.curr_data['trades'] = pl.concat([
                 self.curr_data['trades'],
                 pl.DataFrame(blank_trade, schema_overrides=l1_schema),
@@ -394,9 +398,9 @@ class Replayer:
                 column='Timestamp',
                 descending=False
             )
-    
+
     def _insert_to_start(self, blank_update, blank_trade, l2_schema, l1_schema):
-        # insert a blank message to start of trades/l2 data 
+        # insert a blank message to start of trades/l2 data
         self.curr_data['l2'] = self.curr_data['l2'].set_sorted(
             column='Timestamp',
             descending=False
@@ -411,36 +415,46 @@ class Replayer:
         ]).set_sorted(
             column='Timestamp',
             descending=False
-        )    
+        )
         self.curr_data['trades'] = pl.concat([
             pl.DataFrame(blank_trade, schema_overrides=l1_schema),
             self.curr_data['trades'],
         ]).set_sorted(
             column='Timestamp',
             descending=False
-        )    
-        
+        )
+
     def __repr__(self) -> str:
         return f"Replayer({self.dir}, {self.eid}, {self.freq})"
-    
+
     def init_params(self):
         if self.universe == []:
             self.universe = self.curr_data['l2']['Code'].unique().to_list()
             self.universe = [code for code in self.universe if code != 'blank']
-            self.blank_update_template['Code'] = copy.deepcopy(self.universe)
-            self.blank_trade_template['Code'] = copy.deepcopy(self.universe)
+        self.blank_update_template['Code'] = copy.deepcopy(self.universe)
+        self.blank_trade_template['Code'] = copy.deepcopy(self.universe)
+        for key in self.blank_trade_template.keys():
+            if key != 'Code':
+                self.blank_trade_template[key] = [None] * len(self.universe)
+        for key in self.blank_update_template.keys():
+            if key != 'Code':
+                self.blank_update_template[key] = [None] * len(self.universe)
         self.ob_container = {code: LocalOrderBook(code) for code in self.universe}
         self.trade_handler_container = {code: TradesHandler(code, self.freq) for code in self.universe}
         self.time = datetime.datetime.strptime(self.date, "%Y-%m-%d") - datetime.timedelta(hours=2)
-        self.dest_file_streams = {code: os.path.join(self.dest, f"{code}.csv") for code in self.universe}
+        self.dest_file_streams = {
+            code: open(os.path.join(self.dest, f"{code}.csv"), "w+", buffering=self.buffer_size)
+            for code in self.universe
+        }
+        print(self.dest_file_streams.keys())
         features = [f'bid_price_{i}' for i in range(10)] + [f'bid_qty_{i}' for i in range(10)] +\
                    [f'ask_price_{i}' for i in range(10)] + [f'ask_qty_{i}' for i in range(10)] +\
-                   ['open', 'high', 'low', 'close', 'volume', 'amount', 'timestamp']
+                   ['open', 'high', 'low', 'close', 'volume', 'amount'] +\
+                   all_features + ['timestamp']
         features = ', '.join(features)
         for dest in self.dest_file_streams.values():
-            with open(dest, 'w+') as dest:
-                dest.write(f"{features}\n")
-    
+            dest.write(f"{features}\n")
+
     def list_dates(self, dir) -> list:
         assert os.path.isdir(dir), f"{dir} is not a directory"
         dates = set()
@@ -451,4 +465,3 @@ class Replayer:
         dates = sorted(list(dates))
         dates = [d for d in dates if d >= self.start]
         return dates
-
