f618d326f9cf534bce69762498484e0222aea6a1
jiale0402 <55820495+jiale0402@users.noreply.github.com>
2024-04-17 19:50:57 -0500

    update check_ob.py

--- check_ob.py
+++ check_ob.py
@@ -4,7 +4,7 @@ def check_ob(ob_handler, bid_limits, ask_limits):
     local_ob_bid_volumes = ob_handler.bid_volumes
     local_ob_ask_prices = ob_handler.ask_prices
     local_ob_ask_volumes = ob_handler.ask_volumes    
-    for i in range(len(bid_limits)):
+    for i in range(min(len(bid_limits), len(ask_limits))):
         bid_price_diff = local_ob_bid_prices[i] - bid_limits[i][0]
         bid_volume_diff = local_ob_bid_volumes[i] - bid_limits[i][1]
         ask_price_diff = local_ob_ask_prices[i] - ask_limits[i][0]
